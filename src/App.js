062e6f81911d5e1ad38ea15781755e817feba484
Yaroslav <yaroslavkovalc03@gmail.com>
2026-04-14 21:54:23 +0300

    practice

--- src/App.js
+++ src/App.js
@@ -1,13 +1,23 @@
 import React, { useState } from 'react';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
+import './styles/App.css';
+import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
-  const [value, setValue] = useState('text');
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'Javascript', body: 'Description' },
+    { id: 2, title: 'Javascript 2', body: 'Description' },
+    { id: 3, title: 'Javascript 3', body: 'Description' },
+  ]);
 
   return (
     <div className='App'>
-      <ClassCounter />
+      <form>
+        <input type='text' placeholder='post title'></input>
+        <input type='text' placeholder='post description'></input>
+        <MyButton disabled={true}>create a post</MyButton>
+      </form>
+      <PostList posts={posts} title='Posts about JavaScript' />
     </div>
   );
 }
--- src/components/ClassCounter.jsx
+++ src/components/ClassCounter.jsx
@@ -4,6 +4,9 @@ class ClassCounter extends React.Component {
   constructor(props) {
     super(props);
     this.state = { count: 0 };
+
+    this.increment = this.increment.bind(this);
+    this.decrement = this.decrement.bind(this);
   }
 
   increment() {
